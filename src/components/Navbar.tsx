f181b19b33fa3dcd5215629849fbe57a027a7b33
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-17 13:19:23 +0100

    Modified Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,36 +3,37 @@ import { FaBars, FaMoon } from "react-icons/fa";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <nav>
-      <div className="flex flex-col md:flex-row justify-center bg-neutral-300 p-2">
-        <button
-          className="flex md:hidden justify-self-end self-end"
-          onClick={() => setToggle((prevState) => !prevState)}
-        >
-          <FaBars />
-        </button>
-        <ul
-          className={`md:flex flex-col ${
-            toggle ? "flex" : "hidden"
-          } md:flex-row gap-2 md:gap-16 justify-center items-center text-sm font-normal
+    <nav
+      className="flex max-w-min rounded-full top-5 flex-col md:flex-row justify-center p-2 fixed bg-transparent backdrop-blur-md shadow-[0_0_3px_rgba(0,0,0,0.3)] z-10
+     "
+    >
+      <button
+        className="flex md:hidden justify-self-end self-end"
+        onClick={() => setToggle((prevState) => !prevState)}
+      >
+        <FaBars />
+      </button>
+      <ul
+        className={`md:flex flex-col ${
+          toggle ? "flex" : "hidden"
+        } md:flex-row gap-2 md:gap-16 justify-center items-center text-sm font-normal
           `}
-        >
-          <li>
-            <button className="bg-blue-500 p-2 rounded-full text-white">
-              <FaMoon />
-            </button>
-          </li>
-          <li>About Me</li>
-          <li>Portfolio</li>
-          <li>Skills</li>
-          <li>Resume</li>
-          <li>
-            <button className="bg-blue-500 py-2 px-3 rounded-full text-white">
-              Contact
-            </button>
-          </li>
-        </ul>
-      </div>
+      >
+        <li>
+          <button className="bg-blue-500 p-3 rounded-full text-white">
+            <FaMoon />
+          </button>
+        </li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Skills</li>
+        <li>Resume</li>
+        <li>
+          <button className="bg-blue-500 py-2 px-3 rounded-full text-white">
+            Contact
+          </button>
+        </li>
+      </ul>
     </nav>
   );
 };
