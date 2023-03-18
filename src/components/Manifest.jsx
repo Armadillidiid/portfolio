34c9037f6dc40f1f23b124e469c9c33dde630f56
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-18 12:36:03 +0100

    Created Manifest component

---
+++ src/components/Manifest.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Manifest = () => {
+  return (
+    <div className="flex justify-center items-center h-[50vh] gradient-text">
+      <p className="text-7xl text-center tracking-tighter leading-[75px] font-bold">
+        Dedicated to crafting exceptional digital experiences which inspire
+        positive change
+      </p>
+    </div>
+  );
+};
+
+export default Manifest;
