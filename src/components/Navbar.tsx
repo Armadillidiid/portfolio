a12e0ddcfaca43f225f2d427d4b3f14167c34d18
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-21 22:25:52 +0100

    Added Nav links respective urls and animated scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars, FaMoon } from "react-icons/fa";
+import { animateScroll, Link, scrollSpy } from "react-scroll";
 
 const Navbar: React.FC = () => {
   const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    scrollSpy.update();
+  }, []);
+
   return (
     <nav
       className="flex bg-white/50 max-w-min rounded-full top-5 flex-col md:flex-row justify-center p-2 fixed backdrop-blur-[8px] shadow-[0_0_3px_rgba(0,0,0,0.3)] z-10
@@ -25,13 +31,38 @@ const Navbar: React.FC = () => {
             <FaMoon />
           </button>
         </li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Skills</li>
-        <li>Resume</li>
+        <li className="cursor-pointer">
+          <Link to="about" spy={true} smooth={true} offset={0} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link to="skills" spy={true} smooth={true} offset={0} duration={500}>
+            Skills
+          </Link>
+        </li>
         <li>
           <button className="bg-blue-500 py-2 px-3 rounded-full text-white">
-            Contact
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              Contact
+            </Link>
           </button>
         </li>
       </ul>
