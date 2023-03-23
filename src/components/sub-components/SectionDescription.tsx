efeb10d24b44715fb6cb9d6e68da083e9842ecf3
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-23 21:23:03 +0100

    Added prop to accept style class

--- src/components/sub-components/SectionDescription.tsx
+++ src/components/sub-components/SectionDescription.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  style?: string;
 }
 
-const SectionDescription = ({ children }: Props) => {
+const SectionDescription = ({ children, style }: Props) => {
   return (
-    <h3 className="text-8xl dark:text-white font-semibold tracking-tighter mb-12">
+    <h3 className={`${style} text-8xl dark:text-white font-semibold tracking-tighter mb-12`}>
       {children}
     </h3>
   );
--- src/components/sub-components/SectionTitle.tsx
+++ src/components/sub-components/SectionTitle.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from "react";
 
 interface Prop {
   children: ReactNode;
+  style?: string;
 }
 
-const SectionTitle = ({ children }: Prop) => {
+const SectionTitle = ({ children, style }: Prop) => {
   return (
-    <h3 className="text-3xl font-semibold text-neutral-700 dark:text-white mb-4">{children}</h3>
+    <h3 className={`${style} text-3xl font-semibold text-neutral-700 dark:text-white mb-4`}>{children}</h3>
   );
 };
 
