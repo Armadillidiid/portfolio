bd65c1724f158f153bc618bf7f20d4a19d2ca1cd
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-26 14:40:41 +0100

    Changed z-index

--- src/components/sub-components/ThemeToggle.tsx
+++ src/components/sub-components/ThemeToggle.tsx
@@ -31,11 +31,11 @@ const ThemeToggle = ({ navbar }: Prop) => {
           className="blue-button active:scale-90 duration-100 p-3 rounded-full transition"
           onClick={handleClick}
         >
-          {theme === "light" ? <FaMoon /> : <FaSun />}
+          {theme === "light" ? <FaMoon /> : <BsFillSunFill />}
         </button>
       ) : (
         <button
-          className="flex md:hidden items-center gap-2 font-medium text-sm bg-white drop-shadow-[0_0px_3px_rgba(0,0,0,0.35)] p-3 rounded-full bottom-0 z-50"
+          className="flex md:hidden items-center gap-2 font-medium text-sm bg-white drop-shadow-[0_0px_3px_rgba(0,0,0,0.35)] p-3 rounded-full bottom-0 z-10"
           onClick={handleClick}
         >
           {theme === "light" ? (
